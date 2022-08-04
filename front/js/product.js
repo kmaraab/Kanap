c1ab60b711634378cb22eed45d5b3f0e805f302a
kmaraab <aboubacarcamara0110@gmail.com>
2022-08-04 20:07:58 +0200

    add product to cart

--- front/js/product.js
+++ front/js/product.js
@@ -1,3 +1,5 @@
+/////////////////////// Lien entre un produit de la page d’accueil et la page Produit ///////////////////////////
+
 let idProduit = new URL(location.href).searchParams.get("id"); //recupère la valeur de l'id du lien
 
 (async function(){
@@ -5,8 +7,8 @@ let idProduit = new URL(location.href).searchParams.get("id"); //recupère la va
     addProductToProductPage(product);
 })()
 
-//RECUPERATION DES DONNEES D'UN PRODUIT DEPUIS L'API
-function getProduct(){
+//récupération des données d'un produit depuis l'api
+async function getProduct(){
     return fetch('http://localhost:3000/api/products/' + idProduit)
         .then(function(res){
             if(res.ok){
@@ -21,7 +23,9 @@ function getProduct(){
         })    
 }
 
-//AJOUT D'UN PRODUIT A LA PAGE PRODUIT
+
+
+//insertion d'un produit et ses détails dans la page produit
 function addProductToProductPage(product){
     let imageProduct = document.createElement('img');
                 imageProduct.alt = product.altTxt;
@@ -52,4 +56,107 @@ function addProductToProductPage(product){
     
     document.getElementById('description')
         .innerText = descrisptionProduct;
+}
+
+
+
+
+///////////////////////////  Ajout d'un produit au panier /////////////////////
+
+let addToCart = document.getElementById('addToCart');
+let cart = [];
+
+
+//classe de nouveau produit a ajouter au panier
+class addNewProductToCart {
+    constructor(id, urlImg, name, quantity, color, price, total){
+        this.id = id;
+        this.urlImg = urlImg;
+        this.name = name;
+        this.quantity = quantity;
+        this.color = color;
+        this.price = price;
+        this.total = total;
+    }
+}
+
+
+
+(async function(){
+    const product = await getProduct();
+    let idProductAddToCart = await getIdProductAddToCart(product);
+    let urlImgProductAddToCart = await getUrlImgProductAddToCart(product);
+    let nameProductAddToCart = await getNameProductAddToCart(product);
+    let priceProductAddToCart = await getPriceProductAddToCart(product);
+    pressAddToCart(idProductAddToCart, urlImgProductAddToCart, nameProductAddToCart, priceProductAddToCart);
+})()
+
+
+
+//récuperation de l'id du produit
+function getIdProductAddToCart(product){
+    return product._id;
+}
+
+
+//récuperation de l'URL de l'image du produit
+function getUrlImgProductAddToCart(product){
+    return product.imageUrl;
+}
+
+
+//récuperation du nom du produit
+function getNameProductAddToCart(product){
+    return product.name;
+}
+
+
+//récuperation du prix du produit
+function getPriceProductAddToCart(product){
+    return product.price;
+}
+
+
+//ajout d'un produit dans le local storage
+function pressAddToCart(idProductAddToCart, urlImgProductAddToCart, nameProductAddToCart, priceProductAddToCart){
+    addToCart.addEventListener('click',function(event){
+        
+        
+        // ecoute la modification de la couleur
+        let getColor = document.getElementById('colors');
+        getColor.addEventListener('change', function(event){
+            let colorSelect = getColor.options[getColor.selectedIndex].text;
+            getColor.value = colorSelect; 
+            event.stopPropagation();
+        });
+        let colorSelect = getColor.value;
+        
+
+        // ecoute la modification de la qauntité
+        let getQuantity = document.getElementById('quantity');
+        getQuantity.addEventListener('change', function(event){
+            let newQuantity = getQuantity.value;
+            getQuantity.value = newQuantity; 
+            event.stopPropagation();
+        });
+        let quantityValue = getQuantity.value;
+
+
+        // création d'un nouveau objet de produit à ajouter au panier
+        let productAddToCart = new addNewProductToCart(idProductAddToCart, urlImgProductAddToCart, nameProductAddToCart, quantityValue, colorSelect, priceProductAddToCart, quantityValue*priceProductAddToCart);
+        
+        //verif s'il y'a des articles dans le panier on les recupère avant d'ajouter le nouveau
+        if(localStorage.getItem("Cart")){
+            cart = JSON.parse(localStorage.getItem("Cart"));
+            cart.push(productAddToCart);
+            let stringCart = JSON.stringify(cart);
+            localStorage.setItem("Cart", stringCart);
+          }
+          else{
+            cart.push(productAddToCart);
+            let stringCart = JSON.stringify(cart);
+            localStorage.setItem("Cart", stringCart);
+          }
+        event.stopPropagation;
+    })
 }
--- front/js/script.js
+++ front/js/script.js
@@ -1,9 +1,13 @@
+///////////////////////// Insertion des produits dans la page d’accueil ////////////////////
+
 (async function(){
     const products = await getProducts();
     addProductsToHome(products);
 })()
 
-//RECUPERATION DES DONNEES DES PRODUITS DEPUIS L'API
+
+
+//recuperation des donnees des produits depuis l'api
 function getProducts(){
     return fetch('http://localhost:3000/api/products')
         .then(function(res){
@@ -20,22 +24,32 @@ function getProducts(){
 }
 
 
-//AJOUT DES PRODUITS A LA PAGE D'ACCUEIL
+
+//ajout des produits a la page d'accueil
 function addProductsToHome(products){
     let i = 0;
     while (i < products.length){
+
+        //ajout des l'id dans les lien des produits
         let lienProduct = document.createElement('a');
             lienProduct.href = "./product.html" + "?id=" + products[i]._id;
 
+        
         let contentProduct = document.createElement('article');
+            
+            //recuperation des URL des produits
             let imageProduct = document.createElement('img');
                 imageProduct.alt = products[i].altTxt;
                 imageProduct.src = products[i].imageUrl;
 
+
+            //recuperation des noms des produits    
             let nomProduct = document.createElement('h3');
                 nomProduct.classList.add("productName");
                 nomProduct.innerText = products[i].name; 
 
+
+            //recuperation des descriptions des produits    
             let descrisptionProduct = document.createElement('p');
                 descrisptionProduct.classList.add("productDescription");
                 descrisptionProduct.innerText = products[i].description;
